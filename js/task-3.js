05e56fced96d4eb3bea01e28db29cfe17f5da989
dovgenik <dovgenik@gmail.com>
2025-01-20 15:13:01 +0200

    hw3

--- js/task-3.js
+++ js/task-3.js
@@ -1,18 +1,17 @@
-function checkForSpam(message) {
-  let rezult= false;
-  
-  if ((message.toLowerCase().indexOf("spam") != -1) || (message.toLowerCase().indexOf("sale") != -1)) {
-   rezult= true; 
-  } 
-  
-  return rezult;
+function filterArray(numbers, value) {
+  let rezultArray = [];
+
+  for (const el of numbers) {
+    if (el > value) {
+      rezultArray.push(el);
+    }
+  }
+  return rezultArray;
 }
 
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
